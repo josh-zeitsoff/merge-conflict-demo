978817e053898d953e80464c47186e38a72b4f82
Joshua Zeitsoff <jzeitsoff@berkeley.edu>
2017-06-25 19:00:34 -0700

    Initial commit

---
+++ MergeConflict.java
@@ -0,0 +1,11 @@
+public class MergeConflict {
+
+	int numMerges;
+
+	public MergeConflict() {
+		this.numMerges = 0;
+	}
+	public static void main(String[] args) {
+		MergeConflict mc = new MargeConflict();
+	}
+}
